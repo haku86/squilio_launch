8503a06732586051c5d83b64062703a54d71642d
Joel Tan <lomyril86@gmail.com>
2012-01-15 23:06:19 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SquilioLaunch::Application.config.secret_token = 'ed56a385c57884ec805d5c3babd737718da2abcd1feb476355e0c982a006b5c631a18bc68490f6f5843f6269d7d524c5a708838ba6df7c749739dc9d2a092183'
